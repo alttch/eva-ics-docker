4e0f9e9f629fa76785ec5db03ccb2483d7da61c2
divisor <div@altertech.com>
2019-05-16 01:48:32 +0200

    test repo

--- download.sh
+++ download.sh
@@ -1,10 +1,16 @@
 #!/bin/sh
 
-VER=`curl -s https://get.eva-ics.com/update_info.json|jq -r .version`
-BUILD=`curl -s https://get.eva-ics.com/update_info.json|jq -r .build`
+REPO=https://get.eva-ics.com
+
+if [ "x$1" = "x--test" ]; then
+  REPO=https://test.eva-ics.com
+fi
+
+VER=`curl -s ${REPO}/update_info.json|jq -r .version`
+BUILD=`curl -s ${REPO}/update_info.json|jq -r .build`
 
 rm -f build/eva-dist.tgz
 rm -f run/eva-dist.tgz
 
-wget https://get.eva-ics.com/${VER}/stable/eva-${VER}-${BUILD}.tgz -O build/eva-dist.tgz || exit 1
+wget ${REPO}/${VER}/stable/eva-${VER}-${BUILD}.tgz -O build/eva-dist.tgz || exit 1
 cp -v build/eva-dist.tgz run/
