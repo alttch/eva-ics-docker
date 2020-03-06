2080f4dfa8e649553558ac59539f542b5db958b7
Sergei S <div@altertech.com>
2020-03-06 01:25:36 +0100

    download.sh

--- download.sh
+++ download.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/sh -e
 
 REPO=https://get.eva-ics.com
 
@@ -12,5 +12,5 @@ BUILD=`curl -s ${REPO}/update_info.json|jq -r .build`
 rm -f build/eva-dist.tgz
 rm -f run/eva-dist.tgz
 
-wget ${REPO}/${VER}/stable/eva-${VER}-${BUILD}.tgz -O build/eva-dist.tgz || exit 1
-cp -v build/eva-dist.tgz run/
+wget ${REPO}/${VER}/stable/eva-${VER}-${BUILD}.tgz -O build/eva-dist.tgz
+cp -vf build/eva-dist.tgz run/
