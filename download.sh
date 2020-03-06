69b3238054137eba376f4551460228cb313ed01f
Sergei S <div@altertech.com>
2020-03-06 01:51:10 +0100

    download.sh

--- download.sh
+++ download.sh
@@ -6,11 +6,11 @@ if [ "x$1" = "x--test" ]; then
   REPO=https://test.eva-ics.com
 fi
 
-VER=`curl -s ${REPO}/update_info.json|jq -r .version`
-BUILD=`curl -s ${REPO}/update_info.json|jq -r .build`
+VER=`curl -sL ${REPO}/update_info.json|jq -r .version`
+BUILD=`curl -sL ${REPO}/update_info.json|jq -r .build`
 
 rm -f build/eva-dist.tgz
 rm -f run/eva-dist.tgz
 
-wget ${REPO}/${VER}/stable/eva-${VER}-${BUILD}.tgz -O build/eva-dist.tgz
+wget ${REPO}/${VER}/nightly/eva-${VER}-${BUILD}.tgz -O build/eva-dist.tgz
 cp -vf build/eva-dist.tgz run/
