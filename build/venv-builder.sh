384fb61d8214c42b14c05b07c22d91eac005d7db
Sergei S <div@altertech.com>
2021-06-12 02:02:44 +0200

    3.4

--- build/venv-builder.sh
+++ build/venv-builder.sh
@@ -2,10 +2,11 @@
 
 cd /opt
 
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh /dev/stdin -y
+source $HOME/.cargo/env
 tar xzvf eva-dist.tgz || exit 1
 find . -type d -name "eva*" -maxdepth 1 -exec mv -f {} eva \;
 cd eva
 echo "SYSTEM_SITE_PACKAGES=1" > /etc/venv
 echo "SKIP=\"pandas\"" >> /etc/venv
-sed -i 's/cryptography==.*/cryptography==3.0/g' /opt/eva/install/mods.list
 ./install/build-venv /
